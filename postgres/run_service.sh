6869a7e029672bbf3fb4c1cd3d0afac220cd2432
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-03-06 23:55:46 +0100

    feat: postgres image

--- postgres/run_service.sh
+++ postgres/run_service.sh
@@ -3,6 +3,8 @@
 if [ ! -d "$PG_DATA_FOLDER" ] || [ -z "$(ls -A "$PG_DATA_FOLDER")" ]; then
     mkdir "$PG_DATA_FOLDER"
     initdb --pgdata="$PG_DATA_FOLDER" --encoding="$ENCODING" --username="$USER"
+    cp /tmp/postgresql.conf.backup "$PG_DATA_FOLDER"/postgresql.conf
+    cp /tmp/pg_hba.conf.backup "$PG_DATA_FOLDER"/pg_hba.conf
 fi
 
 pg_ctl start -D /var/lib/postgresql/data
