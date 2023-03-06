693e80ead9df3f1498036500f6efa0291a9d3e53
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-03-06 23:43:08 +0100

    feat: postgres image

--- postgres/run_service.sh
+++ postgres/run_service.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-pg_ctl start -D /var/lib/postgresql/data -l /var/log/postgresql/postgres.log
-tail -f /var/log/postgresql/postgres.log
+if [ ! -d "$PG_DATA_FOLDER" ] || [ -z "$(ls -A "$PG_DATA_FOLDER")" ]; then
+    mkdir "$PG_DATA_FOLDER"
+    initdb --pgdata="$PG_DATA_FOLDER" --encoding="$ENCODING" --username="$USER"
+fi
+
+pg_ctl start -D /var/lib/postgresql/data
+tail -F /var/log/postgresql/postgresql-*.json
