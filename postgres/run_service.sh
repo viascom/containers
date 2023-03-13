a03c76e3625af0ccedab53dcb530c88a7229c3d6
Nikola Stankovic <nikola.stankovic@viascom.ch>
2023-03-13 23:18:19 +0100

    feat: postgres image prepare for multi container use

--- postgres/run_service.sh
+++ postgres/run_service.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$PG_DATA_FOLDER" ] || [ -z "$(ls -A "$PG_DATA_FOLDER")" ]; then
     mkdir "$PG_DATA_FOLDER"
-    initdb --pgdata="$PG_DATA_FOLDER" --encoding="$ENCODING" --username="$USER" --pwfile=<(echo $PG_SUPER_USER_PASSWORD)
+    initdb --pgdata="$PG_DATA_FOLDER" --encoding="$PG_DEFAULT_DATABASE_ENCODING" --username="$PG_SUPER_USER_NAME" --pwfile=<(echo $PG_SUPER_USER_PASSWORD)
     cp /tmp/postgresql.conf.backup "$PG_DATA_FOLDER"/postgresql.conf
     cp /tmp/pg_hba.conf.backup "$PG_DATA_FOLDER"/pg_hba.conf
 fi
